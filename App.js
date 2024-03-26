e4ea0efbe86a13df2541ea39478c768e0414019f
geronilton <geronilton001@gmail.com>
2024-03-26 11:34:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+import { TextInput } from 'react-native-web';
 
-export default function App() {
+const CatApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{width:250,height:400, textAlign:'center'}}>
+      <Image
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
+
+        <view>
+          <Text style={{margin:50}}>Login</Text>
+          <TextInput 
+            style={{
+              borderWidth:1,borderRadius:10
+            }}
+          placeholder="E-mail"
+          textContentType="name"></TextInput>
+        </view>
+
+        <view>
+          <Text style={{margin:50}}>Senha</Text>
+          <TextInput 
+          style={{
+            borderWidth:1,borderRadius:10
+          }}
+          placeholder="Password"
+          secureTextEntry={true}
+          textContentType="password"></TextInput>
+        </view>
+        <button>Entrar</button>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default CatApp;
