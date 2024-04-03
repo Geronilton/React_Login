3e5fe00661c70d54c81403045317d71f25fac0d0
geronilton <geronilton001@gmail.com>
2024-04-03 17:07:56 -0300

    acrescentei formulario de cadastro

--- App.js
+++ App.js
@@ -1,79 +1,173 @@
-import React from 'react';
-import {Text, View, Image} from 'react-native';
-import { TextInput } from 'react-native-web';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-function Login(){
-  return(
-    <View style={{width:250, marginLeft:60, marginTop:"60px"}}>
-      <View   style={{width: 200, height: 200, marginLeft:20}}>
-        <Image
-        source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
-        }}
-        style={{width: 200, height: 200}}
-      />
-      </View>
-      <view style={{margin:10}}>
-        <Text style={{margin:80}}>Login</Text>
-        <TextInput 
-          style={{
-            borderWidth:1,borderRadius:10, width:'230px',height:'30px',textAlign:'center'
-          }}
-        placeholder="E-mail"
-        textContentType="name"></TextInput>
-      </view>
-
-      <view style={{margin:10}}>
-        <Text style={{margin:80}}>Senha</Text>
-        <TextInput 
-        style={{
-          borderWidth:1,borderRadius:10, width:'230px',height:'30px',textAlign:'center'
-        }}
-        placeholder="Password"
-        secureTextEntry={true}
-        textContentType="password"></TextInput>
-      </view>
-      <button style={{width:100, height:'30px',
-      borderRadius:10,marginLeft:75,
-      background:'blue', color:'white'}} 
-      onPress={() =>{HomeScreen}}>Entrar</button>
-  </View>
-  );
-}
+import React, { useState } from 'react';
 
 
 const Drawer = createDrawerNavigator();
+
+
+function LoginScreen({ navigation }) {
+
+  const [getLogin, setLogin] = useState('');
+  const [getPass, setPass] = useState('');
+
+  return (
+    <View style={styles.ScreenLogin}>
+      <View style={styles.containerLogin}>
+        <View style={styles.img}>
+          <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
+            }}
+            style={{ width: 200, height: 200 }}
+          />
+        </View>
+
+        <Text style={styles.formText}> Login:</Text>
+        <TextInput style={styles.formTextInput}
+          value={getLogin} onChangeText={text => setLogin(text)}></TextInput>
+
+
+        <Text style={styles.formText}>Password:</Text>
+        <TextInput style={styles.formTextInput}
+          value={getPass} onChangeText={pass => setPass(pass)}></TextInput>
+
+
+        <View style={styles.botao}>
+          <Button title="Ok"
+            onPress={() => {
+              navigation.navigate('DashBoard', {
+                login: getLogin
+              });
+            }
+            } />
+        </View>
+        <View style={styles.botao}>
+          <Button title="Cadastrar"
+            onPress={() => {
+              navigation.navigate('Cadastro');
+            }
+            } />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+
+function Cadastro({ navigation }) {
+  return (
+    <View style={styles.ScreenCadastro}>
+      <View style={styles.cadastro}>
+        <Text style={styles.titleCadastro}>Cadastre-se</Text>
+
+        <Text style={styles.formText}> Nome:</Text>
+        <TextInput style={styles.formTextInput}></TextInput>
+
+        <Text style={styles.formText}> Sobrenome:</Text>
+        <TextInput style={styles.formTextInput}></TextInput>
+
+        <Text style={styles.formText}> E-mail:</Text>
+        <TextInput style={styles.formTextInput}></TextInput>
+
+        <Text style={styles.formText}> Telefone:</Text>
+        <TextInput style={styles.formTextInput}></TextInput>
+        
+        <View>
+          <Button title='Cadastrar' />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+
+
+
+function DashBoardScreen({ navigation, route }) {
+  const login = route.params.login;
+
+
+  return (
+    <View>
+      <Text> Você esta logado como {JSON.stringify(login)} </Text>
+    </View>
+  );
+}
+
+
+
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Login">
-      <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}></Drawer.Screen>
+        <Drawer.Screen name='DashBoard' component={DashBoardScreen}></Drawer.Screen>
+        <Drawer.Screen name='Cadastro' component={Cadastro} ></Drawer.Screen>
 
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ScreenLogin: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLogin: {
+    borderRadius: 15,
+    width: '90%',
+  },
+  formText: {
+    fontSize: 20,
+    margin: 5,
+  },
+  formTextInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 7,
+    height: 30,
+  },
+  img: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  botao: {
+    marginLeft: '30%',
+    margin: 5,
+    width: '130px',
+  },
+  ScreenCadastro: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cadastro:{
+    width:'75%',
+  },
+  titleCadastro:{
+    fontSize: 35,
+    margin: 10,
+  }
+});
+
+
+
